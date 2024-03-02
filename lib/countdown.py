1db4332ae130ac072637f7a2bfc647a54a4bf338
irene <irenejim9504@gmail.com>
2024-03-02 15:54:28 +0300

    Added countdown_with_sleep function

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,22 @@
-# your code goes here!
+
+
+
+
+def countdown(num):
+    while num > 0:
+        print(f'{num} SECOND(S)!')
+        num -= 1
+countdown(20)
+print("HAPPY NEW YEAR!")
+
+
+import time
+
+def countdown_with_sleep(num):
+    while num > 0:
+        print(f'{num} SECOND(S)!')
+        time.sleep(1) 
+        num -= 1
+countdown(10)
+print("HAPPY NEW YEAR!")
+
